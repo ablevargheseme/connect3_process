13bb9abfbc49bde7da9ee2e0e0152c9da583cdd4
ARK-ASWINRAJ <aswinrajkailath@gmail.com>
2024-03-31 02:09:33 +0530

    modified with gql query to subgraph

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+const TelegramBot = require("node-telegram-bot-api");
 
-
-const TelegramBot = require('node-telegram-bot-api');
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 const { MongoClient } = require("mongodb");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config(); // Load environment variables from .env file
 
@@ -13,32 +13,58 @@ let client = null; // Variable to hold the MongoDB client instance
 
 const bot = new TelegramBot(token, { polling: true });
 
+export const Transfer_Query = `
+     query{
+        transfers (first:10){
+            id
+            from
+            to
+            value
+            blockNumber
+            blockTimestamp
+            transactionHash
+          }
+      }
+`;
+const endpoint =
+  "https://api.studio.thegraph.com/query/69594/connect3/version/latest";
+const apolloClient = new ApolloClient({
+  uri: endpoint,
+  cache: new InMemoryCache(),
+});
 async function connectToDatabase() {
-    try {
-        if (!client || !client.topology.isConnected()) {
-            client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-            console.log("Connected to MongoDB");
-        }
-    } catch (err) {
-        console.error("Error connecting to MongoDB:", err);
+  try {
+    if (!client || !client.topology.isConnected()) {
+      client = await MongoClient.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("Connected to MongoDB");
     }
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+  }
 }
 
 async function fetchData() {
-    try {
-        await connectToDatabase();
-        const db = client.db(databaseName);
-        const collection = db.collection("eventmodels");
-        const ans = await collection.find({}).toArray();
-        let i = 0;
-        ans.forEach((element) => {
-            i++;
-            console.log(`target address index:${i}`, element.address);
-            bot.sendMessage(`${element.actionValue}`, 'Hello, this is a message from your Telegram bot.');
-        });
-    } catch (err) {
-        console.error("Error fetching data:", err);
-    }
+  try {
+    await connectToDatabase();
+    const db = client.db(databaseName);
+    const collection = db.collection("eventmodels");
+    const ans = await collection.find({}).toArray();
+    let i = 0;
+    ans.forEach(async (element) => {
+      i++;
+      console.log(`target address index:${i}`, element.address);
+      let data = await client.query({ query: gql(Transfer_Query) });
+      bot.sendMessage(
+        `${element.actionValue}`,
+        "Hello, this is a message from your Telegram bot."
+      );
+    });
+  } catch (err) {
+    console.error("Error fetching data:", err);
+  }
 }
 
 // Call fetchData every 10 seconds
